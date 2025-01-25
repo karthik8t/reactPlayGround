e8a7522cbfa5a4762134b3d8026eb9d78f6eafd7
karthik8t <thatikondakarthik13@gmail.com>
2025-01-25 20:00:28 +0530

    completed travelJournal static page project

--- staticpage/src/App.tsx
+++ staticpage/src/App.tsx
@@ -1,12 +1,17 @@
 import {useState} from 'react'
 import './App.css'
 import FactCard from './factCard/components/FactCard'
+import TravelJournal from "@/travelJournal/TravelJournal.tsx";
+import {ThemeProvider} from "@/components/theme/theme-provider.tsx";
 
 function App() {
     const [count, setCount] = useState(0)
 
     return (
-        <FactCard/>
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+            <TravelJournal/>
+        </ThemeProvider>
+        // <FactCard/>
     )
 }
 
