2ad7ccdaac4298643f074be14da91186734a47df
karthik8t <thatikondakarthik13@gmail.com>
2025-01-26 17:58:17 +0530

    handle add ingredients with local variable

--- ai-chef/src/components/ingredients/Ingredients.tsx
+++ ai-chef/src/components/ingredients/Ingredients.tsx
@@ -3,22 +3,31 @@ import {Input} from "@/components/ui/input.tsx";
 import {Button} from "@/components/ui/button.tsx";
 import {cn} from "@/lib/utils.ts";
 
+
+let ingredients : string[] = ["Onions", "Tomatoes", "Eggs"]
+
+const submitIngredient = (event: Event) => {
+    event.preventDefault();
+    const formData : FormData = new FormData(event.currentTarget);
+    let ingredient : string = formData.get("ingredient");
+    ingredients.push(ingredient);
+    console.log(ingredients)
+}
+
 type Props = {};
 export default function Ingredients({}: Props) {
     return (
         <Card className={cn("w-7/12 h-5/6 my-auto bg-[#FBFBFB] flex flex-col items-center max-w-[75rem]")}>
             <CardHeader className={"w-full"}>
-                <div className={"flex justify-center mt-12"}>
-                    <Input type={"text"} placeholder={"ingredient"} enterKeyHint={"enter"} className={"w-1/2"}/>
-                    <Button type={"button"} className={"ml-4"}>Add</Button>
-                </div>
+                <form className={"flex justify-center mt-12"} onSubmit={submitIngredient}>
+                    <Input type={"text"} placeholder={"ingredient"} enterKeyHint={"enter"} className={"w-1/2"} name={"ingredient"}/>
+                    <Button type={"submit"} className={"ml-4"}>Add</Button>
+                </form>
             </CardHeader>
             <CardContent className={"w-3/4 mx-auto mt-16 p-0"}>
                 <h1 className={"text-4xl"}>Ingredients in hand: </h1>
                 <ul className={"text-2xl list-disc mt-4 ml-12"}>
-                    <li>Onions</li>
-                    <li>Chilli</li>
-                    <li>Apple</li>
+                    {ingredients.map((ingredient, index) => (<li key={index}>{ingredient}</li>) )}
                 </ul>
             </CardContent>
             <CardFooter className={cn("mt-auto mb-12 h-32 bg-[#FFF5D7] w-11/12 rounded-xl flex items-center p-0 justify-center shadow-2xl border max-w-[60rem]")}>
