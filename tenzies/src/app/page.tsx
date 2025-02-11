6d7588e04c07d39e8541c64ba1dce0fca692ac3e
karthik8t <thatikondakarthik13@gmail.com>
2025-02-11 18:05:52 +0530

    init

--- tenzies/src/app/page.tsx
+++ tenzies/src/app/page.tsx
@@ -1,7 +1,22 @@
+import {Calendar1, ClockArrowUp} from 'lucide-react';
+import Image from "next/image";
+import {Button} from "@/components/ui/button";
+
 export default function Home() {
+
   return (
-      <div>
-        hello worlds
+      <>
+      <div className={"mx-4 mt-2"}>
+          <div className={"flex"}><ClockArrowUp/> <span className={"ml-3"}>{new Date().toLocaleDateString()}</span></div>
+          <div className={"flex mt-2"}><Calendar1 /> <span className={"ml-3"}>{new Date().toLocaleTimeString()}</span></div>
+          <div className={"w-[150] h-[100] bg-no-repeat bg-contain invert mt-3"} style={{"backgroundImage": "url('/spike.png')"}}></div>
       </div>
-  );
+          <div>
+              <div>
+                  <Button size={"image"} className={""}></Button>
+              </div>
+
+          </div>
+      </>
+          );
 }
