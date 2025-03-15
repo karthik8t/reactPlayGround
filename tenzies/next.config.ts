b41259dbb33c521db60213c3dc4ed04c8e1988cf
karthik8t <thatikondakarthik13@gmail.com>
2025-03-16 04:20:50 +0530

    modified git-pages

--- tenzies/next.config.ts
+++ tenzies/next.config.ts
@@ -2,7 +2,7 @@ import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
     output: "export",
-    basePath: "/tenzies",
+    basePath: "/reactPlayGround/tenzies",
   /* config options here */
     serverRuntimeConfig: {
         PROJECT_ROOT: __dirname
--- tenzies/src/components/tenzies/AgentGrid.tsx
+++ tenzies/src/components/tenzies/AgentGrid.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import {Agent, Button} from "@/components/ui/button";
+import {Button} from "@/components/ui/button";
 import {AgentTile, Player, PlayerType} from "@/lib/Models";
 
 
@@ -8,7 +8,7 @@ type Props = {
     attacker: Player,
     defender: Player,
     currentPlayer: PlayerType,
-    lockTile: any
+    lockTile: (id: string, agent: string) => void
 };
 export default function AgentGrid({attacker, defender, currentPlayer, lockTile}: Props) {
     const player = currentPlayer === 'attack' ? attacker : defender;
@@ -19,7 +19,7 @@ export default function AgentGrid({attacker, defender, currentPlayer, lockTile}:
             <div className={"grid grid-cols-5 gap-2 w-max p-3 bg-[#ecfffe20]"}>
             {player.tiles.map((tile: AgentTile) => {
                 return (<Button size={"agent"}
-                                agent= {tile.agent as unknown as Agent}
+                                agent= {tile.agent}
                                 key={tile.uniqueId}
                                 tile={tile.isLocked?"locked" :"default"}
                                 onClick={() => lockTile(tile.uniqueId, tile.agent)}></Button>)
